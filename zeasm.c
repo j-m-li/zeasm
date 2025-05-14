2f4c1441c716ec482b0ccd0d05a66a9c68db822a
o-ksi-d <none@mail.com>
2025-05-14 11:33:01 +0200

    Z

--- zeasm.c
+++ zeasm.c
@@ -35,25 +35,255 @@
  (32bit RISC-V, 62kB Flash, 20kB SRAM, USB host/device, 48MHz) 
 */
 
-#include "std.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define byte unsigned char
+#define var long
+
+#define MAX_ID_LEN 70
 
 byte buf[512];
 byte input[512];
+var ahead = 0;
+byte tmp[MAX_ID_LEN+1];
+
+var comment(var c);
+var next()
+{
+	var t = ahead;
+	ahead = getchar();
+	return t;
+}
+
+var error(byte *str)
+{
+	printf("#error (%c)  %s",ahead,  str);
+	exit(-1);
+}
+
+var identifier()
+{
+	var i = 0;
+	while (i < sizeof(buf) - 1 && ahead != EOF) {
+		if ((ahead >= 'a' && ahead <= 'z') || 
+				(ahead >= '0' && ahead <= '9')) 
+		{
+			buf[i] = ahead;
+			i++;
+		} else {
+			break;
+		}
+		next();
+	}
+	if (i < 1) {
+		error("identifier");
+	}
+	buf[i] = 0;
+	return (var)buf;
+}
+
+
+var kcmp(byte *key, byte *tmp)
+{
+	var c;
+	var i = 0;
+	while (tmp[i]) {
+		if (tmp[i] != key[i]) {
+			return tmp[i] - key[i];
+		}
+		i++;
+	}
+	if (!key[i]) {
+		return 0;
+	}
+	while (key[i] && ahead != EOF && i < MAX_ID_LEN) {
+		if (ahead >= 'a' && ahead <= 'z') {
+		        if (ahead == key[i]) {	
+				tmp[i] = next();
+				i++;
+			} else {
+				break;
+			}
+		} else {
+			break;
+		}
+	}
+	tmp[i] = 0;
+	if (!key[i]) {
+		return 0;
+	}	
+	return -1;
+}
+
+var spaces()
+{
+	while (ahead != EOF) {
+		switch (ahead) {
+		case ' ':
+		case '\t':
+		case '\r':
+		case '\n':
+			break;
+		case '?':
+			break;
+		case '!':
+			comment(next());
+			break;
+		default:
+			return 0;
+		}
+		next();
+	}
+	return 0;
+}
+
+
+var string()
+{
+	var i;
+	var c;
+	if (ahead != '"') {
+		error("string");
+		return 0;
+	}
+	next();
+	i = 0;
+	while (i < sizeof(buf) - 1 && (c = next()) != EOF) {
+		if (c == '"') {
+			buf[i] = 0;
+			return (var)buf;
+		}
+		buf[i] = c;
+		i++;
+	}
+	error("string no end");
+	return 0;
+}
+
+var include()
+{
+	byte *s;
+	spaces();
+	s = (byte*)string();
+	printf("#include \"%s.h\"\n", s);
+	return 0;
+}
+
+var func()
+{
+	byte *s;
+	spaces();
+	s = (byte*)identifier();
+	printf("var %s(", s);
+	spaces();
+	while (ahead != EOF && ahead != ':' && ahead != ';') {
+		s = (byte*)identifier();
+		spaces();
+		if (ahead == ',') {
+			printf("var %s, ", s);
+			next();
+		} else {
+			printf("var %s", s);
+			break;
+		}
+	}
+	if (ahead == ':') {
+	} else if (ahead == ';') {
+		printf(");\n");
+		return 0;
+	} else {
+		error("missing ; or :");
+	}
+	printf(")\n{\n");
+	printf("}\n");
+	return 0;
+}
+
+
+var keyword(var c)
+{
+	var i;
+	tmp[0] = c;
+	tmp[1] = 0;
+	switch (c) {
+	case 'i':
+		if (!kcmp("include", tmp)) {
+			return include();
+		}
+		break;	
+	case 'f':
+		if (!kcmp("func", tmp)) {
+			return func();
+		}
+		break;	
+	default:
+	}
+	i = 0;
+	while (tmp[i]) { 
+		i++; 
+	}
+	while (ahead != EOF && i < MAX_ID_LEN) {
+		if (ahead >= 'a' && ahead <= 'z') {
+			tmp[i] = next();
+			i++;
+		} else {
+			break;
+		}
+	}
+	tmp[i] = 0;
+	printf("UNK %s\n", tmp);
+	
+	return -1;
+}
+
+
+var comment(var c)
+{
+	while ((c = next()) != EOF) {
+		switch (c) {
+		case '\n':
+		case '\r':
+			return c;
+		case '?':
+			c = next();
+			if (c == '\r') {
+				next();
+			}
+			break;
+		}
+	}
+	return -1;
+}
 
 var process(var argc, byte **argv)
 {
 	var c, l;
 	l = 0;
-	while ((c = getchar()) != EOF) {
+	c = -1;
+	next();
+	while ((c = next()) != EOF) {
 		if (l < 511) {
 			input[l] = c & 0xFF;
 			l++;
 		}
-		if (c == '\n') {
-			input[l] = '\0';
-			printf(">%s", input);
-			fflush(stdout);
-			l = 0;
+		switch (c) {
+		case '!':
+			comment(c);
+			break;
+		case '\n':
+		case '\r':
+		case ' ':
+		case '\t':
+			break;
+		default:
+			if (c >= 'a' && c <= 'z') {
+				 keyword(c);
+			} else {
+				printf("%c", c);
+				fflush(stdout);
+			}
 		}
 	}
 	return 0;
