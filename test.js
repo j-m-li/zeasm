dff60a9dbf771da86a6b2bf5318245e3dd66884d
o-ksi-d <none@mail.com>
2025-05-30 20:03:45 +0200

    js

--- test.js
+++ test.js
@@ -1,60 +1,99 @@
-// Assume add.wasm file exists that contains a single function adding 2 provided arguments
 const fs = require('node:fs');
+const tm = require('node:timers/promises');
 
-// Use the readFileSync function to read the contents of the "add.wasm" file
-const wasmBuffer = fs.readFileSync('test.cmd');
+var wasm;
+var term;
 
-// Use the WebAssembly.instantiate method to instantiate the WebAssembly module
-
-var wasmInstance;
-
-function log(ptr)
+function mem2string(ptr)
 {
-     let len = 0;
-     let hl = 0;
-     const head = new Uint8Array(wasmInstance.exports.memory.buffer, ptr, 4);
-     if (head[0] < 0xC0) {
-	len = head[0];
-	hl = 1;
-     } else if (head[0] < 0xE0) {
-	len = head[0] & 0x1F;
-	len <<= 6;
-	len += head[1] & 0x3F;
-	hl = 2;
-     } else if (head[0] < 0xF0) {
-	len = head[0] & 0x0F;
-	len <<= 6;
-	len += head[1] & 0x3F;
-	len <<= 6;
-	len += head[2] & 0x3F;
-	hl = 3;
-     } else {
-	len = head[0] & 0x07;
-	len <<= 6;
-	len += head[1] & 0x3F;
-	len <<= 6;
-	len += head[2] & 0x3F;
-	len <<= 6;
-	len += head[3] & 0x3F;
-	hl = 4;
-     }
+     	let len = 0;
+     	let hl = 0;
+     	const head = new Uint8Array(wasm.exports.memory.buffer, ptr, 4);
+     	if (head[0] < 0xC0) {
+		len = head[0];
+		hl = 1;
+     	} else if (head[0] < 0xE0) {
+		len = head[0] & 0x1F;
+		len <<= 6;
+		len += head[1] & 0x3F;
+		hl = 2;
+     	} else if (head[0] < 0xF0) {
+		len = head[0] & 0x0F;
+		len <<= 6;
+		len += head[1] & 0x3F;
+		len <<= 6;
+		len += head[2] & 0x3F;
+		hl = 3;
+     	} else {
+		len = head[0] & 0x07;
+		len <<= 6;
+		len += head[1] & 0x3F;
+		len <<= 6;
+		len += head[2] & 0x3F;
+		len <<= 6;
+		len += head[3] & 0x3F;
+		hl = 4;
+     	}
+      	const bytes = new Uint8Array(wasm.exports.memory.buffer, ptr + hl, len);
+      	return new TextDecoder('utf-8').decode(bytes);
+}
 
-     const bytes = new Uint8Array(wasmInstance.exports.memory.buffer, ptr + hl, len);
-      const text = new TextDecoder('utf-8').decode(bytes);
-      console.log(text);
+function print(ptr)
+{
+	process.stdout.write(mem2string(ptr));
+}
+
+function keyboard(data)
+{
+	let seq = data.toString();
+	if (seq == "\r") {
+		seq = "\r\n";
+	}
+	if (seq == "\x1B") {
+		process.exit(0);
+	}
+        let u = new TextEncoder('utf-8').encode(seq);
+	let s = u.length;
+	let a = new Uint32Array(wasm.exports.memory.buffer, term, 6);
+	let si = new Uint8Array(wasm.exports.memory.buffer, term + 4 * 6, 1);
+	let d = new Uint8Array(wasm.exports.memory.buffer, term + 4 * 6+1, 127);
+	if (s > 127) {
+		s = 127;
+	}
+	si[0] = s;
+	for (i = 0; i < s; i++) {
+		d[i] = u[i]
+	}
+	a[3] = 1; /* event type */
+	a[4] = s; /* event data length */
+	a[5] = term + 4 * 6; /* event data pointer */
+	wasm.exports.event(term);
+}
+
+function timeout()
+{
+        tm.setTimeout(1000).then(timeout);
+	let d = new Uint8Array(wasm.exports.memory.buffer, term + 4 * 6, 1);
+	d[0] = 0;
+	wasm.exports.event(term);
 }
 
 const imp = {
-	console: {
-		log: log,
+	std: {
+		print: print,
 	},
 };
 
+const wasmBuffer = fs.readFileSync('test.cmd');
 WebAssembly.instantiate(wasmBuffer.slice(128), imp).then(wasmModule => {
-  wasmInstance = wasmModule.instance;
-  // Exported function lives under instance.exports object
-  const { add } = wasmModule.instance.exports;
-  const sum = add(40, 10);
-  console.log("40 + 10 = " + sum); // Outputs: 11
+	wasm = wasmModule.instance;
+	process.stdin.setRawMode(true);
+  	term = wasm.exports.term__new();
+   	tm.setTimeout(100).then(timeout);
+	process.stdin.on('data', keyboard);
+
+	const { add } = wasmModule.instance.exports;
+  	const sum = add(40, 10);
+  	console.log("40 + 10 = " + sum); // Outputs: 11
 });
 
--- test.wat
+++ test.wat
@@ -1,5 +1,5 @@
 (module 
-   (import "console" "log" (func $log (param i32)))
+   (import "std" "print" (func $print (param i32)))
    (memory (export "memory") 1)
    (data (i32.const 0) "\0DHello, 世界!")
    (data (i32.const 16) "\01!")
@@ -12,10 +12,22 @@
       i32.add
 
       i32.const 0 ;; string ptr (4 bytes aligned)
-      call $log
+      call $print
       i32.const 16
-      call $log
+      call $print
    ) 
+   (func $event
+      (param $a i32) 
+      i32.const 20 ;; offset of term class
+      i32.const 24 ;; offset of data buffer
+      i32.add
+      call $print
+   )
+   (func $term__new (result i32)
+      i32.const 20 
+   )
    (export "add" (func $add))
+   (export "term__new" (func $term__new))
+   (export "event" (func $event))
 )
 
